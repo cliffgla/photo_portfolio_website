02764329340c20f3406d3760d85ca6ec588e75ea
cliffgla <cliffordgagliardo@gmail.com>
2017-11-02 15:11:51 +0000

    show all button added. I think this code is not as DRY as it could be

--- scripts.js
+++ scripts.js
@@ -1,5 +1,21 @@
 $("#DTLA-button").on("click", function() {
     if ($("img").hasClass("DTLA")) {
-        $("img").css("display", "none");
+        $(".food").css("display", "none");
+        $(".portrait").css("display", "none");
+
     }
+});
+
+$("#food-button").on("click", function() {
+    if ($("img").hasClass("DTLA")) {
+        $(".food").css("display", "none");
+
+    }
+});
+
+$("#show-all-button").on("click", function() {
+
+        $("img").css("display","inline" );
+
+
 });
